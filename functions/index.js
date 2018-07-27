c9657d6c6e9c6221f60dd56a365b774752a70b2d
Eddie Chen <agameofprivacy@gmail.com>
2018-07-27 14:29:36 +0800

    updated conversation trigger functions

--- functions/index.js
+++ functions/index.js
@@ -182,85 +182,23 @@ exports.onPackageDeletion = functions.firestore.document('packages/{packageID}')
     const index = client.initIndex(ALGOLIA_PACKAGES_INDEX_NAME);
     return index.deleteObject(context.params.packageID);
 });
-// update package current location on algolia when it's updated
-
-
-
-// Update the search index every time a blog post is written.
-// exports.onPackageLocationUpdated = functions.firestore.document('packages/{packageId}').onWrite((change, context) => {
-//     // Get the note document
-//     const updatedPackageData = change.after.data();
-//     let categoriesArray = [];
-//     for (var key in updatedPackageData['categories']) {
-//         // check if the property/key is defined in the object itself, not in parent
-//         if (updatedPackageData['categories'].hasOwnProperty(key)) {
-//             categoriesArray.push(key);
-//         }
-//     }
-
-//     const packagePreview = {
-//         'tagName': updatedPackageData['tag']['name'],
-//         'headline': updatedPackageData['headline'],
-//         'recipientName': updatedPackageData['recipient']['name'],
-//         'moversCount': updatedPackageData['count']['movers'],
-//         'followersCount': updatedPackageData['count']['followers'],
-//         'destination': updatedPackageData['destination'],
-//         'origin': {
-//             'lat': updatedPackageData['origin']['geo_point'].latitude,
-//             'lng': updatedPackageData['origin']['geo_point'].longitude
-//         },
-//         '_geoloc': {
-//             'lat': updatedPackageData['_geoloc'].latitude,
-//             'lng': updatedPackageData['_geoloc'].longitude
-//         },
-//         'dueDate': Math.round(new Date(updatedPackageData['due_date']['end']).getTime() / 1000),
-//         '_tags': categoriesArray,
-//         'status': updatedPackageData['status'],
-//     };
-
-//     // Add an 'objectID' field which Algolia requires
-//     packagePreview.objectID = context.params.packageId;
-
-//     // Write to the algolia index
-//     const index = client.initIndex(ALGOLIA_PACKAGES_INDEX_NAME);
-//     return index.saveObject(packagePreview);
-// });
-
-// Update tags everytime a write operation is performed on a topic(tag)
-// exports.onTopicUpdated = functions.firestore.document('topics/{topicId}').onWrite((change, context) => {
-//     // Get the note document
-//     const updatedTopicData = change.after.data();
-
-//     const topic = {
-//         'tag': updatedTopicData['tag'],
-//         'templatesCount': updatedTopicData['count']['templates'],
-//         'packagesCount': updatedTopicData['count']['packages'],
-//     };
-
-//     // Add an 'objectID' field which Algolia requires
-//     topic.objectID = change.after.ref.id;
-
-//     // Write to the algolia index
-//     const index = client.initIndex(ALGOLIA_TOPICS_INDEX_NAME);
-//     return index.saveObject(topic);
-// });
 
 // Update topic conversation index on conversation creation
-exports.onConversationCreated = functions.firestore.document('topics/{topicId}/conversations/{conversationId}').onCreate((snapshot, context) => {
+exports.onConversationCreated = functions.firestore.document('topics/{topicId}/conversations/{conversationID}').onCreate((snapshot, context) => {
     const data = snapshot.data();
     const key = Object.keys(data["legislative_area"])[0];
     const value = data["legislative_area"][key];
     var newConversation = {
-        "objectID": snapshot.ref.id,
+        key: value,
     }
-    newConversation[key] = value;
+    newConversation.objectID = context.params.conversationID;
     // Write to the algolia index
     const index = client.initIndex(ALGOLIA_TOPIC_CONVERSATION_INDEX_NAME);
     return index.saveObject(newConversation);
 });
 
 // remove topic from algolia index when it is deleted
-exports.onConversationDeletion = functions.firestore.document('topics/{topicID}/conversations/{conversationId}').onDelete((snapshot, context) => {
+exports.onConversationDeletion = functions.firestore.document('topics/{topicID}/conversations/{conversationID}').onDelete((snapshot, context) => {
     const index = client.initIndex(ALGOLIA_TOPIC_CONVERSATION_INDEX_NAME);
-    return index.deleteObject(context.params.conversationId);
+    return index.deleteObject(context.params.conversationID);
 });
